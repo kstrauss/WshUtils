c229083b057bb5cadd285f2add27aa9e7528a50c
Karl Strauss <kstrauss@realgy.com>
2014-09-30 12:44:14 -0400

    update deploy script to sign if the signing script is available

--- Modules/VisualStudioProjectModifiers/deployME.ps1
+++ Modules/VisualStudioProjectModifiers/deployME.ps1
@@ -1,2 +1,15 @@
-copy VisualStudioProjectModifiers.psm1 ~\Documents\WindowsPowerShell\Modules\VisualStudioProjectModifiers
-copy VisualStudioModifier.psd1 ~\Documents\WindowsPowerShell\Modules\VisualStudioProjectModifiers
+$destDir = "~\Documents\WindowsPowerShell\Modules\VisualStudioProjectModifiers"
+$filesToCopy = @("VisualStudioProjectModifiers.psm1","VisualStudioModifier.psd1")
+$filesToCopy | %{
+	copy $_ $destDir
+}
+
+if (get-command sign-script){
+    $filesToCopy | %{
+	    $codeToSign = Join-Path $destDir $_
+        sign-script $codeToSign
+    }
+    else{
+        Write-Warning "Did not sign module because Sign-Script command was not found"
+    }
+}
